df860ef58e38d0cf55adeb780577d1e703688f44
Nabil <nabil.tharwat@outlook.com>
2021-04-27 00:55:43 +0200

    config: jest coverage from untested files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
 	clearMocks: true,
 	coverageDirectory: './coverage',
 	testEnvironment: 'node',
+	collectCoverageFrom: ['./lib/**/*.js'],
 	coverageReporters: ['json-summary', 'html', 'text', 'lcov'],
 	testMatch: ['./**/*.test.js'],
+	setupFilesAfterEnv: ['./jest.setup.js'],
 }
